d588a379f3727155433871025d8edb8cee580495
sunflowerhjkl <141923679+sunflowerhjkl@users.noreply.github.com>
2025-06-28 15:38:14 +0300

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -6,6 +6,50 @@ myParagraph.textContent = "The Reisen";
 
 const img = document.querySelector("img");
 const header = document.querySelector("h1");
+const body = document.querySelector("body");
+const main_body = document.querySelector("main_body");
+const login_body = document.querySelector("login_body");
+let button = document.querySelector("button");
+const input_button = document.querySelector("input_button");
+const input = document.querySelector("input");
+
+login_body.hidden = true;
+
+function showLoginWindow() {
+    main_body.hidden = true;
+    login_body.hidden = false;
+}
+function hideLoginWindow() {
+    main_body.hidden = false;
+    login_body.hidden = true;
+}
+
+function setSoulName() {
+    //const name = prompt("Shall we know thy name?");
+    const name = input.value;
+    /*
+    if (!name)
+        setSoulName();
+    else */
+    if (name) {
+        hideLoginWindow();
+        localStorage.setItem("name", name);
+        header.textContent = `Be hallowed thy name, ${name}!`;
+    }
+}
+
+if (!localStorage.getItem("name")) {
+    //setSoulName()
+    showLoginWindow();
+}
+else {
+    const name = localStorage.getItem("name");
+    header.textContent = `Be hallowed thy name, ${name}!`;
+}
+
+header.addEventListener("click", () => {
+    header.textContent = "The Reisen";
+});
 
 img.addEventListener("click", () => {
     const src = img.getAttribute("src");
@@ -13,30 +57,18 @@ img.addEventListener("click", () => {
         img.setAttribute("src", "src/minecraft.jpg");
     else
         img.setAttribute("src", "src/reisen.jpg");
-}
-);
-
-header.addEventListener("click", () => {
-    header.textContent = "The Reisen";
-})
-
-let button = document.querySelector("button");
-function setSoulName() {
-    const name = prompt("Shall we know thy name?");
-    if (!name)
-        setSoulName();
-    else {
-        localStorage.setItem("name", name);
-        header.textContent = `Be hallowed thy name, ${name}!`;
-    }
-}
-if (!localStorage.getItem("name"))
-    setSoulName()
-else {
-    const name = localStorage.getItem("name");
-    header.textContent = `Be hallowed thy name, ${name}!`;
-}
+});
 
 button.addEventListener("click", () => {
+    //setSoulName();
+    showLoginWindow();
+});
+
+input_button.addEventListener("click", () => {
     setSoulName();
 });
+
+
+
+
+
